9897bb2e6fe6ba520b7aa81be976d0c257f520fd
NicoBobBison <ndejesse@u.rochester.edu>
2023-11-04 16:25:36 -0400

    Data base working

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return render_template('home.html')
-
-
-def complete_task():
-    return True
+    todo_list = Todo.query.all()
+    return render_template('home.html', todo_list=todo_list)
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        db.session.add(Todo(task_name="Do the dishes", complete=False))
+        db.session.commit()
     app.run(debug=True)
