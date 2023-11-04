d8604d3a89c4190ae53abfaa4f33e00347c00a48
Erik Enriquez <erikenriquez34@gmail.com>
2023-11-04 18:38:21 -0400

    text change, removal, taskID (no backend connect yet)

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,16 +1,35 @@
 const taskSection = document.querySelector(".checklist-section")
+let taskId = 0;
 
 taskSection.addEventListener("click", function (e) {
     if(e.target.tagName === "BUTTON") {
         if (e.target.className === "uncheckedBox"){
             e.target.classList.replace("uncheckedBox", "checkedBox")
-            e.target.classList.replace("uncheckedText", "checkedText")
+            e.target.nextElementSibling.classList.replace("uncheckedText", "checkedText")
         } else {
             e.target.classList.replace("checkedBox", "uncheckedBox")
-            e.target.classList.replace("checkedText", "uncheckedText")
+            e.target.nextElementSibling.classList.replace("checkedText", "uncheckedText")
         }
     }
-    else if (e.target.tagName === "SPAN") {
-        alert("also")
+    else if (e.target.className === "uncheckedText") {
+        alert("instructions to edit task name")
+    }
+    else if (e.target.id === "trash-can") {
+        e.target.parentElement.parentElement.parentElement.remove();
     }
 }, false)
+
+function addTask() {
+    alert("creating task with: " + taskId)
+
+    // let li = document.createElement("li");
+    // li.innerHTML = inputBox.value;
+    // listContainer.appendChild(li);
+    // inputBox.value = "";
+    //
+    // let span = document.createElement("span");
+    // span.innerHTML = "\u00d7";
+    // li.appendChild(span);
+
+    taskId = taskId + 1;
+}
