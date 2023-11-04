79a18a83e3098818970eeef85ecb7adb774b5f88
NicoBobBison <ndejesse@u.rochester.edu>
2023-11-04 17:45:11 -0400

    Button switches images

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,11 +1,13 @@
-const taskSection = document.querySelector(".task-section")
+const taskSection = document.querySelector(".checklist-section")
 
 taskSection.addEventListener("click", function (e) {
     if(e.target.tagName === "BUTTON") {
         if (e.target.className === "uncheckedBox"){
             e.target.classList.replace("uncheckedBox", "checkedBox")
+            e.target.classList.replace("uncheckedText", "checkedText")
         } else {
             e.target.classList.replace("checkedBox", "uncheckedBox")
+            e.target.classList.replace("checkedText", "uncheckedText")
         }
     }
     else if (e.target.tagName === "SPAN") {
